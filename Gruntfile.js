8ab305227ef72b93863b90c609e0cd8b4de90196
necromman <necromman@gmail.com>
2018-11-14 16:13:10 +0900

    update

---
+++ Gruntfile.js
@@ -0,0 +1,8 @@
+module.exports = function(grunt) {
+    require('time-grunt')(grunt);
+    require('grunt-contrib-watch')(grunt);
+    grunt.initConfig({
+
+    });
+    grunt.registerTask('dev', []);
+};
