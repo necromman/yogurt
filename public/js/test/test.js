f9477d284274a583157056fc22c2d45e805248bd
necromman <necromman@gmail.com>
2018-11-19 13:27:07 +0900

    update

--- public/js/test/test.js
+++ public/js/test/test.js
@@ -1,10 +1,10 @@
-var Now = new Date();
+/*var Now = new Date();
 var NowTime = Now.getFullYear();
 NowTime += '-' + leadingZeros((Now.getMonth() + 1), 2);
 NowTime += '-' + leadingZeros(Now.getDate(), 2);
 NowTime += ' ' + leadingZeros(Now.getHours(), 2);
 NowTime += ':' + leadingZeros(Now.getMinutes(), 2);
-NowTime += ':' + leadingZeros(Now.getSeconds(), 2);
+NowTime += ':' + leadingZeros(Now.getSeconds(), 2);*/
 
 /*
 *==================================================================
@@ -29,6 +29,7 @@ function leadingZeros(n, digits) {
 */
 var tDay, tHour, tMinute, tSecond = 0;
 var fromDate = "240000";
+
 function updateClock() {
     var now = moment();
     var eventTime = moment(fromDate, "hhmmss");
@@ -45,19 +46,39 @@ function updateClock() {
     console.log("카운트다운 시간" + leadingZeros(tHour, 2));
     console.log("카운트다운 분" + leadingZeros(tMinute, 2));
     console.log("카운트다운 초" + leadingZeros(tSecond, 2));
-    var mSecond = parseInt($.trim(now).substr(10,1));
+    var mSecond = parseInt($.trim(now).substr(10, 1));
     var mSecondTemp;
-    switch (mSecond){
-        case 1 : mSecondTemp = 9; break;
-        case 2 : mSecondTemp = 8; break;
-        case 3 : mSecondTemp = 7; break;
-        case 4 : mSecondTemp = 6; break;
-        case 5 : mSecondTemp = 5; break;
-        case 6 : mSecondTemp = 4; break;
-        case 7 : mSecondTemp = 3; break;
-        case 8 : mSecondTemp = 2; break;
-        case 9 : mSecondTemp = 1; break;
-        default : mSecondTemp = 0; break;
+    switch (mSecond) {
+        case 1 :
+            mSecondTemp = 9;
+            break;
+        case 2 :
+            mSecondTemp = 8;
+            break;
+        case 3 :
+            mSecondTemp = 7;
+            break;
+        case 4 :
+            mSecondTemp = 6;
+            break;
+        case 5 :
+            mSecondTemp = 5;
+            break;
+        case 6 :
+            mSecondTemp = 4;
+            break;
+        case 7 :
+            mSecondTemp = 3;
+            break;
+        case 8 :
+            mSecondTemp = 2;
+            break;
+        case 9 :
+            mSecondTemp = 1;
+            break;
+        default :
+            mSecondTemp = 0;
+            break;
     }
     console.log("카운트다운 밀리" + mSecondTemp);
     /*
@@ -68,13 +89,54 @@ function updateClock() {
     */
 }
 
-var clear = setInterval(function () {
+/*var clear = setInterval(function () {
     updateClock();
     if (tHour <= 0 && tMinute <= 0 && tSecond <= 0) {
         clearInterval(clear);
         console.log("카운트다운 없음");
     }
-}, 100);
+}, 100);*/
 
 
+/*
+*==================================================================
+*  다음주 일요일 구하기 날짜 구하기
+*==================================================================
+*/
+week = ['일', '월', '화', '수', '목', '금', '토'];
+var NowT;
+for (var i = 0; i < 7; i++) {
+    var Now = new Date();
+    var B = new Date(Now.setDate(Now.getDate()+ 7 +i)).getDate();
+    if (B == 0) {
+        break;
+    }
+}
 
+NowT = Now.getFullYear();
+NowT += '-' + (Now.getMonth() + 1);
+NowT += '-' + B;
+console.log("다음주 일요일은 " + NowT);
+
+
+NowT = Now.getFullYear();
+NowT += '-' + leadingZeros((Now.getMonth() + 1), 2);
+NowT += '-' + leadingZeros(B, 2);
+console.log("다음주 일요일은 " + NowT);
+
+
+/*
+*==================================================================
+*  다음주 일요일 구하기 요일 구하기
+*==================================================================
+*/
+week = ['일', '월', '화', '수', '목', '금', '토'];
+for (var i = 0; i < 7; i++) {
+    var Now = new Date();
+    var B = new Date(Now.setDate(Now.getDate()+ 7 +i)).getDay();
+    if (B == 0) {
+        break;
+    }
+}
+console.log(B);
+console.log(week[B]);
